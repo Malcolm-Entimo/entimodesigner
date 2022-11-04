59530b1dbc50946e6d6ad82c0ee5355d63fadf48
Malcolm-Entimo <malcolm@ptouchonline.nl>
2022-11-04 10:53:01 +0100

    modified:   cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "m9nsta",
+  projectId: "cc4c5184-0efc-47b0-8098-d91050fe2c60",
   // The rest of the Cypress config options go here...
   e2e: {
     setupNodeEvents(on, config) {
