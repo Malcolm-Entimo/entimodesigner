27d76aa5cc929d0952e44da482a177309e5ee79d
Malcolm-Entimo <malcolm@ptouchonline.nl>
2022-11-04 11:26:01 +0100

    modified:   cypress.config.js 	new file:   cypress/support/component-index.html 	new file:   cypress/support/component.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "m9nsta",
+
   // The rest of the Cypress config options go here...
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
 });
